b31a497e44f286ddf2471da973a599f7a5ce7a8b
yein <ehowldpdls@gmail.com>
2025-07-09 00:46:03 +0900

    프론트 패키지 구조 및 설정 완료

---
+++ eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react', 'prettier'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended',
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  };
