ef03dd2ab199b8738eda3d85ad05a62c7934e136
yein <ehowldpdls@gmail.com>
2025-07-11 18:06:35 +0900

    #3 공통 footer ui 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,12 @@
 
 import './App.css';
-import Header from './components/common/header/Header';
+import Footer from './components/common/footer/Footer';
 
 function App() {
  
 
   return (
-    <Header />
+    <Footer/>
   )
 }
 
