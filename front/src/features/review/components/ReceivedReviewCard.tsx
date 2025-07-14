88865e8f0185d369222f3494b903de3a97c0a4ef
yein <ehowldpdls@gmail.com>
2025-07-15 00:12:09 +0900

    #30 신고모달 props 전달받아 이벤트함수 추가

--- front/src/features/review/components/ReceivedReviewCard.tsx
+++ front/src/features/review/components/ReceivedReviewCard.tsx
@@ -5,16 +5,35 @@ import starEmpty from "../../../assets/images/star_empty.png";
 import type { ReviewType } from "../types/review";
 import { useState } from 'react';
 import ReportModal from '../../../components/common/modal/ReportModal';
+import ConfirmModal from '../../../components/common/modal/ConfirmModal';
 
 
 const ReceivedReviewCard:React.FC<{ review: ReviewType }> = ({ review }) => {
     
-  const [isReportModalOpen, setIsReportModalOpen] = useState(false); // 신고 모달 상태
+const [isReportModalOpen, setIsReportModalOpen] = useState(false); // 신고 모달 상태
+const [isReportCompleteModalOpen, setIsReportCompleteModalOpen] = useState(false); // 신고하기 버튼 클릭시 확인 모달 오픈 여부
 
-  // 신고 클릭 시
+  // 신고 버튼 클릭 시
   const handleReportClick = () => {
     setIsReportModalOpen(true)
   }
+
+  // 신고모달 신고하기 클릭 
+  const handleReportConfirm = (selectedReasons: string[], detailText: string) => {
+    console.log(selectedReasons,detailText);
+    setIsReportModalOpen(false)
+    setIsReportCompleteModalOpen(true);
+  }
+
+  // 신고모달 취소 클릭
+  const handleReportCancel = () => {
+    setIsReportModalOpen(false)
+  }
+
+  // 신고하기 버튼 클릭시 공통 확인 모달 
+  const handleReportCompleteConfirm = () => {
+    setIsReportCompleteModalOpen(false);
+  };
 
     return (
         <rs.ReviewCard>
@@ -48,8 +67,22 @@ const ReceivedReviewCard:React.FC<{ review: ReviewType }> = ({ review }) => {
     
           <rs.ButtonWrap>
             <rs.DeleteButton onClick={handleReportClick}>신고</rs.DeleteButton>
+            {/* 신고모달 오픈 */}
             {isReportModalOpen && (
-              <ReportModal/>
+              <ReportModal 
+               onConfirm={handleReportConfirm}
+               onCancel={handleReportCancel}/>
+            )}
+
+            {/* 신고하기 활성화 버튼 클릭 시 확인 모달 오픈 */}
+            {isReportCompleteModalOpen && (
+              <ConfirmModal
+                isOpen={isReportCompleteModalOpen}
+                content="신고가 접수되었습니다."
+                showCancel={false}
+                confirmText="확인"
+                onConfirm={handleReportCompleteConfirm}
+              />
             )}
           </rs.ButtonWrap>
         </rs.ReviewCard>
