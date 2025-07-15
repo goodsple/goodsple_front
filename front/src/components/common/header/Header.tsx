0bc4adbee5ad0ef0dc9c24f162c8d4f86921d6b4
yein <ehowldpdls@gmail.com>
2025-07-15 11:56:48 +0900

    #2 해더 프로필 조건 추가

--- front/src/components/common/header/Header.tsx
+++ front/src/components/common/header/Header.tsx
@@ -4,28 +4,32 @@ import logoImg from '../../../assets/images/logo.png';
 import chatImg from '../../../assets/images/chat.png';
 import alarmImg from '../../../assets/images/alarm.png';
 import profileImg from '../../../assets/images/default_profile.png';
+
+import UserNav from '../nav/UserNav';
 import { useState } from 'react';
 
 function Header(){
     const [isOpen, setIsOpen] = useState(false);
     const [isLoggedIn, setIsLoggedin] = useState(false);
+    const [userProfileImg, setUserProfileImg] = useState<string | null>(null);
 
-    return(
-        <style.HeaderContainer>
-            <style.HeaderInner>
-            <style.LeftArea>
-          <style.HamburgerButton onClick={() => setIsOpen(!isOpen)} $open={isOpen}>
-            <span />
-            <span />
-            <span />
-          </style.HamburgerButton>
-        </style.LeftArea>
-  
-        <style.CenterArea>
-          <style.Logo src={logoImg} alt="GoodsPle 로고" />
-        </style.CenterArea>
-  
-        <style.RightArea>
+  return (
+    <>
+      <style.HeaderContainer>
+        <style.HeaderInner>
+          <style.LeftArea>
+          <style.HamburgerButton onClick={() => { setIsOpen(!isOpen); setMenuOpen(!menuOpen); }} $open={isOpen}>
+              <span />
+              <span />
+              <span />
+            </style.HamburgerButton>
+          </style.LeftArea>
+
+          <style.CenterArea>
+            <style.Logo src={logoImg} alt="GoodsPle 로고" />
+          </style.CenterArea>
+
+          <style.RightArea>
             {isLoggedIn ? (
             <>
                 <style.IconBox iconType="chat">
@@ -35,8 +39,12 @@ function Header(){
                 <style.IconBox iconType="alarm">
                 <img src={alarmImg} alt="알림" />
                 </style.IconBox>
-                <style.ProfileWrapper>
-                    <style.ProfileIcon src={profileImg} alt="기본 프로필" />
+                <style.ProfileWrapper isDefault={!userProfileImg}>
+                  {userProfileImg ? (
+                    <style.ProfileIcon src={userProfileImg} alt="업로드된 프로필 이미지" />
+                  ):(
+                    <style.ProfileIcon src={profileImg} alt="기본 프로필 이미지" isDefault />
+                  )}
                 </style.ProfileWrapper>
                 <style.LogoutButton>로그아웃</style.LogoutButton>
             </>
@@ -50,6 +58,7 @@ function Header(){
             </style.HeaderInner>
 
       </style.HeaderContainer>
+      </>
     )
 }
 
