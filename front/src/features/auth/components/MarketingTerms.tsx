07ecf4f10e60c1a10eb59505f818353139f16295
yein <ehowldpdls@gmail.com>
2025-07-17 16:35:46 +0900

    #50 회원가입 기능로직 추가 및 백엔드 api 연결

---
+++ front/src/features/auth/components/MarketingTerms.tsx
@@ -0,0 +1,17 @@
+import * as s from './SignUpStyle'
+
+const MarketingTerms = () => {
+
+    return (
+        <s.TermsContent>
+            1. 수신 목적:  <br/>
+            - 굿즈 관련 이벤트, 프로모션, 신규 기능 업데이트 안내<br/>
+            2. 수단:  <br/>
+            - 이메일, 문자(SMS), 앱 푸시 알림 등<br/>
+            3. 보유 및 이용기간:  <br/>
+            - 수신 동의 철회 시까지<br/>
+            ※ 사용자는 언제든지 수신 거부를 선택할 수 있습니다.
+        </s.TermsContent>
+    )
+}
+export default MarketingTerms;
---
+++ front/src/features/auth/components/PrivacyTerms.tsx
@@ -0,0 +1,17 @@
+import * as s from './SignUpStyle'
+
+const PrivacyTerms = () => {
+
+    return (
+        <s.TermsContent>
+        1. 수집 항목:<br/>
+        - 아이디, 비밀번호, 이름, 닉네임, 이메일, 휴대전화 번호, 생년월일, 성별<br/>
+        2. 수집 목적:  <br/>
+        - 회원 식별 및 본인 확인, 서비스 제공 및 개선, 고객 상담 및 민원 처리<br/>
+        3. 보유 및 이용기간:  <br/>
+        - 회원 탈퇴 시까지 보관하며, 관련 법령에 따라 일정 기간 보관 후 파기함<br/>
+        ※ 귀하는 개인정보 수집 및 이용에 대해 동의를 거부할 수 있으며, 이 경우 서비스 이용이 제한될 수 있습니다.
+        </s.TermsContent>
+    )
+}
+export default PrivacyTerms;
