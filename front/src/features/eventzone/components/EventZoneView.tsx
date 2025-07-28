ba0cfde521874d3a9ed9ff88718d7b2c229ee8ca
lurufarm <hsg200528@gmail.com>
2025-07-28 23:52:45 +0900

    #87 view 만드는 중

--- front/src/features/eventzone/components/EventZoneView.tsx
+++ front/src/features/eventzone/components/EventZoneView.tsx
@@ -1,4 +1,7 @@
 import CSS from './eventzone.module.css';
+import reply from '../../../assets/eventImages/reply.png';
+import view from '../../../assets/eventImages/view.png';
+import bookmark from '../../../assets/cateImages/bookmark_deactivated.png';
 
 function EventZoneView() {
   return (<div className={CSS.boardCont}>
@@ -13,10 +16,23 @@ function EventZoneView() {
           <p className={CSS.nickName}>닉네임 공간</p>
           <p className={CSS.vTitle}>제목 공간</p>
           <div className={CSS.vEtc}>
-
+            <p>2025.05.05 13:11</p>
+            <img src={reply}/>
+            <p>3</p>
+            <img src={view}/>
+            <p>123</p>
+            <img src={bookmark}/>
           </div>
         </div>
         <div className={CSS.titleLine} />
+        <div className={CSS.vContent}>
+          <p>어쩌구저쩌궁 내용</p>
+          <div className={CSS.vContentImgs}>
+          </div>
+        </div>
+        <div className={CSS.vReport}>
+          신고하기
+        </div>
       </div>
     </div>);
 }
