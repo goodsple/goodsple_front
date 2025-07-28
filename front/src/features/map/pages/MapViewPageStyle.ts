91c9d291db586c0844047b232a5f2ab920028e89
bbwansu <orm6150@gmail.com>
2025-07-28 22:00:26 +0900

    #92 지도뷰 컴포넌트 및 페이지 초안 추가

---
+++ front/src/features/map/pages/MapViewPageStyle.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const PageLayout = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100vh;
+  padding-top: 70px; /* 헤더 높이만큼 여백 */
+  box-sizing: border-box;
+`;
