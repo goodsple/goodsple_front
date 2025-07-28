acaebba3bac19f0429097c951d35ea4992592a6d
lurufarm <hsg200528@gmail.com>
2025-07-28 18:22:06 +0900

    #87 view 만드는 중

--- front/src/features/eventzone/components/EventZoneView.tsx
+++ front/src/features/eventzone/components/EventZoneView.tsx
@@ -1,22 +1,24 @@
 import CSS from './eventzone.module.css';
 
-function EventZoneView()
-{
-  return (
-    <div className={CSS.boardCont}>
+function EventZoneView() {
+  return (<div className={CSS.boardCont}>
       <p className={CSS.popEventTitle}>
         이벤트존 글 상세보기
       </p>
-      <div className={CSS.writeCont}>
+      <div className={CSS.viewCont}>
         <div className={CSS.viewTitle}>
           <div className={CSS.eventName}>
             [WARNING] HOSHI X WOOZI
           </div>
+          <p className={CSS.nickName}>닉네임 공간</p>
+          <p className={CSS.vTitle}>제목 공간</p>
+          <div className={CSS.vEtc}>
+
+          </div>
         </div>
-        <div className={CSS.titleLine}/>
+        <div className={CSS.titleLine} />
       </div>
-    </div>
-  )
+    </div>);
 }
 
 export default EventZoneView;
