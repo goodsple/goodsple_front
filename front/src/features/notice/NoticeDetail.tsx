a7f6af45adf11cab2b84417febb1cb5747baa55d
BBB924 <qhrud4231@naver.com>
2025-09-18 07:44:11 +0900

    feat: 관리자 계정은 수정일 확인 기능 적용, 본문 폰트 적용 #175

--- front/src/features/notice/NoticeDetail.tsx
+++ front/src/features/notice/NoticeDetail.tsx
@@ -2,21 +2,7 @@ import React, { useEffect, useState } from 'react';
 import * as S from './NoticeDetail.styles';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
-
-// const NoticeDetail = () => {
-//     const navigate = useNavigate();
-
-//     // 추후 실제 데이터 받아올 예정
-//     const title = "[공지] 굿즈플 여름 서비스 점검 안내";
-//     const date = "2025.07.30";
-//     const content = `안녕하세요, 굿즈플입니다.
-
-// 보다 안정적인 서비스를 위해 다음 일정에 서버 점검이 예정되어 있습니다.
-
-// - 점검 일시: 2025년 8월 2일(토) 02:00 ~ 05:00
-// - 점검 내용: 서버 개선 및 보안 업데이트
-
-// 이용에 불편을 드려 죄송합니다. 더 나은 서비스로 보답하겠습니다.`;
+import jwtDecode from 'jwt-decode';
 
 interface NoticeDetailData {
     noticeId: number;
@@ -26,12 +12,25 @@ interface NoticeDetailData {
     noticeUpdatedAt: string;
 }
 
+interface TokenPayload {
+    role: string; // 예: "ADMIN" 또는 "USER"
+}
+
 const NoticeDetail = () => {
     const navigate = useNavigate();
     const { noticeId } = useParams<{ noticeId: string }>();
     const [notice, setNotice] = useState<NoticeDetailData | null>(null);
 
+    const [isAdmin, setIsAdmin] = useState(false);
+
     useEffect(() => {
+        const token = localStorage.getItem('accessToken'); // 저장 위치에 맞게 조정
+        if (token) {
+            const payload: TokenPayload = jwtDecode(token);
+            setIsAdmin(payload.role === 'ADMIN');
+        }
+
+
         const fetchNotice = async () => {
             try {
                 const res = await axios.get<NoticeDetailData>(`/api/user/notices/${noticeId}`);
@@ -57,14 +56,15 @@ const NoticeDetail = () => {
             .padStart(2, "0")}.${d.getDate().toString().padStart(2, "0")}`;
     };
 
-
     return (
         <S.Container>
             <S.TitleSection>
                 <S.MenuLabel>공지사항</S.MenuLabel>
                 <S.Divider />
                 <S.NoticeTitle>{notice.noticeTitle}</S.NoticeTitle>
-                <S.NoticeDate>{formatDate(notice.noticeCreatedAt)}</S.NoticeDate>
+                <S.NoticeDate>작성일: {formatDate(notice.noticeCreatedAt)}</S.NoticeDate>
+                {isAdmin && ( <S.NoticeDate>수정일: {formatDate(notice.noticeUpdatedAt)}</S.NoticeDate>
+                )}
                 <S.Divider />
             </S.TitleSection>
 
