4796e5ccfbdfd34ece3c02b96f7f093146320f4f
yein <ehowldpdls@gmail.com>
2025-11-30 14:49:06 +0900

    메인 배너 높이 디바이스별 유동값 적용

--- front/src/main/components/MainBannerSectionStyle.ts
+++ front/src/main/components/MainBannerSectionStyle.ts
@@ -1,29 +1,16 @@
 import styled from "styled-components";
-
-// 세로가 가로 비율에 맞춰서 좀 긴 버전
-// export const BannerSection = styled.div`
-//   width: 100%;
-//   overflow: hidden;
-//   margin-top: 60px;
-// `;
-
-// export const BannerImage = styled.img`
-//     width: 100%;   
-//     height: auto;    
-//     display: block;
-// `;
-
-//  세로가 넘 길다 좀 조정이 필요하다 싶으면 아래 코드 
-export const BannerSection = styled.div`
+ 
+export const BannerSection = styled.section`
   width: 100%;
-  height: 420px;        
-  overflow: hidden;
   margin-top: 60px;
+  overflow: hidden;
 `;
 
 export const BannerImage = styled.img`
   width: 100%;
-  height: 100%;
-  object-fit: cover;   
+  /* 최소 340px, 최대 500px / 화면 너비에 비례해서 유동 */
+  height: clamp(340px, 32vw, 500px);
+  display: block;
+  object-fit: cover;
   object-position: center top;
 `;
